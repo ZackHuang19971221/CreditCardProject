6d79ec9a0a30afc8d1e0c1ec01a86f16ae03d218
ZackHuang19971221 <125232924+ZackHuang19971221@users.noreply.github.com>
2024-05-31 17:34:40 +0800

    -file adjust

---
+++ CreditCard/src/main/java/com/hotsauce/creditcard/io/manage/DejavooManageData.java
@@ -0,0 +1,11 @@
+package com.hotsauce.creditcard.io.manage;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Dejavoo {
+    private String authKey;
+    private String registerId;
+}
---
+++ CreditCard/src/main/java/com/hotsauce/creditcard/providers/Dejavoo.java
@@ -0,0 +1,2 @@
+package com.hotsauce.creditcard.providers;public class Dejavoo {
+}
